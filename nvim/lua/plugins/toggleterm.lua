c7499a7f59fe6c46ae202c6270f9a65b6dba4a91
surattinon <surattinon.basu@gmail.com>
2024-04-07 02:31:14 +0700

    update nvim

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -1,11 +1,12 @@
 return {
-  {'akinsho/toggleterm.nvim',
-    version = "*",
-    opts = {
-      direction = "horizontal",
-      open_mapping = [[<c-t>]],
-      close_in_exit = "flase",
-    },
-    config = true
-  }
+	{
+		"akinsho/toggleterm.nvim",
+		version = "*",
+		opts = {
+			direction = "horizontal",
+			open_mapping = [[<c-\>]],
+			close_in_exit = "flase",
+		},
+		config = true,
+	},
 }
