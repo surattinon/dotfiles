4f06c5761b212f4683babe57f77e1c40cc188bbf
surattinon <surattinon.basu@gmail.com>
2024-04-07 00:25:15 +0700

    add nvim config

---
+++ nvim/lua/plugins/toggleterm.lua
@@ -0,0 +1,11 @@
+return {
+  {'akinsho/toggleterm.nvim',
+    version = "*",
+    opts = {
+      direction = "horizontal",
+      open_mapping = [[<c-t>]],
+      close_in_exit = "flase",
+    },
+    config = true
+  }
+}
